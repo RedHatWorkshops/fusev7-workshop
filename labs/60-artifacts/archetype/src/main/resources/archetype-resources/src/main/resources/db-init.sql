e0271a44e020ecb9c554b9d5960e1c8fcaeec8e4
weimeilin79 <weimeilin@gmail.com>
2018-04-03 12:25:42 -0400

    remove

---
+++ labs/60-artifacts/archetype/src/main/resources/archetype-resources/src/main/resources/db-init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS nametable (
+nameno VARCHAR(10),
+returnname  VARCHAR(100)
+);
+DELETE FROM nametable;
+INSERT INTO nametable VALUES( '1', 'James');
+INSERT INTO nametable VALUES( '2', 'Christina');
